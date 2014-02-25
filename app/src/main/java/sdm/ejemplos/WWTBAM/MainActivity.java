025825a1c74559067fb0ead7395bd4f3d69afcf6
Mirella <mimarmu1@hotmail.com>
2014-02-25 17:51:02 +0100

    Botones, fondo y activities creadas.

--- app/src/main/java/sdm/ejemplos/WWTBAM/MainActivity.java
+++ app/src/main/java/sdm/ejemplos/WWTBAM/MainActivity.java
@@ -1,19 +1,66 @@
 package sdm.ejemplos.WWTBAM;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+    Button buttonPlay;
+    Button buttonScores;
+    Button buttonSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonPlay = (Button) findViewById(R.id.buttonPlay);
+        buttonScores = (Button) findViewById(R.id.buttonScores);
+        buttonSettings = (Button) findViewById(R.id.buttonSettings);
+
+       buttonPlay.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               mostrarActivity(buttonPlay);
+
+           }
+       });
+
+        buttonSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mostrarActivity(buttonSettings);
+
+            }
+        });
+
+        buttonScores.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mostrarActivity(buttonScores);
+
+            }
+        });
     }
+
+
+
+    public void mostrarActivity(View v){
+        int id = v.getId();
+        if(id == R.id.buttonPlay){
+            Intent i = new Intent(this, PlayActivity.class);
+            startActivity(i);
+        }else if (id == R.id.buttonScores){
+            Intent i = new Intent(this, ScoresActivity.class);
+            startActivity(i);
+        }else if( id == R.id.buttonSettings){
+            Intent i = new Intent(this, SettingsActivity.class);
+            startActivity(i);
+        }
+    }
+
+
+
 }
---
+++ app/src/main/java/sdm/ejemplos/WWTBAM/ScoresActivity.java
@@ -0,0 +1,13 @@
+package sdm.ejemplos.WWTBAM;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ScoresActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scores);
+    }
+}
